b5b8e079dae7efc59da614329d2ce58abad00010
sirswamahesh <sirswamahesh@gmail.com>
2025-07-22 07:05:24 +0530

    URL issue

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.set("trust proxy", 1);
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gyan-base.onrender.com",
     credentials: true,
   })
 );
